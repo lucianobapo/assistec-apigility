b93583adb05d1f0068c6861c46532984f59093d4
Luciano Porto <luciano.bapo@gmail.com>
2016-07-08 08:50:57 -0300

    first commit

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ chdir(dirname(__DIR__));
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
-die(var_dump(file_exists('vendor/autoload.php')));
+die(var_dump(file_exists('../vendor/autoload.php')));
 if (!file_exists('vendor/autoload.php')) {
     throw new RuntimeException(
         'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
