49d1ed6158928b027ea3e8a8c1b5d0c063e3e0ac
Luciano Porto <luciano.bapo@gmail.com>
2016-07-08 08:50:32 -0300

    first commit

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
  */
-die(var_dump(file_exists('vendor/autoload.php')));
+
 /**
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
@@ -14,7 +14,7 @@ chdir(dirname(__DIR__));
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;
 }
-
+die(var_dump(file_exists('vendor/autoload.php')));
 if (!file_exists('vendor/autoload.php')) {
     throw new RuntimeException(
         'Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.'
