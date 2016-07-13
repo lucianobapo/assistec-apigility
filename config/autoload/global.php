3d50032162571ac9b00a321cbdc2cc740db25890
Luciano Porto <luciano.bapo@gmail.com>
2016-07-13 17:11:13 -0300

    first commit

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,10 +15,22 @@ return array(
             ),
         ),
     ),
+    'statuslib' => array(
+        'array_mapper_path' => 'data/statuslib.php',
+    ),
     'zf-mvc-auth' => array(
         'authentication' => array(
-            'map' => array(
-                'assistec\\V1' => 'status',
+            'adapters' => array(
+                'status' => array(
+                    'adapter' => 'ZF\\MvcAuth\\Authentication\\HttpAdapter',
+                    'options' => array(
+                        'accept_schemes' => array(
+                            0 => 'basic',
+                        ),
+                        'realm' => 'api',
+                        'htpasswd' => 'data/.htpasswd',
+                    ),
+                ),
             ),
         ),
     ),
