52a34c8cb4a266897b325e77f51cc01e2dd7c6ee
Luciano Porto <luciano.bapo@gmail.com>
2016-07-13 17:09:07 -0300

    first commit

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,17 @@
 return array(
     'db' => array(
         'adapters' => array(
-            'assistec-backend' => array(),
+            'assistec-backend' => array(
+                'charset' => 'utf8',
+                'database' => 'assistec-backend',
+                'driver' => 'PDO_Mysql',
+                'hostname' => 'localhost',
+                'username' => 'assistec-backend',
+                'password' => 'assistec-backend',
+                'driver_options' => array(
+                    'buffer_results' => true,
+                ),
+            ),
         ),
     ),
     'zf-mvc-auth' => array(
